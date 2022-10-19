0ae7c1039d91e7b3a293a036cfd2253856170f82
yb-nt <notacose@naver.com>
2022-10-20 07:53:14 +0900

    saramin_scrapper_upload

--- saramin.py
+++ saramin.py
@@ -1,11 +1,13 @@
 import requests as req
 from bs4 import BeautifulSoup as soup 
 import re
+import time
+import pandas as pd
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.122 Whale/3.16.138.27 Safari/537.36'}
 
-# keyword = input("input keyword: ")
-keyword='데이터 엔지니어'
+keyword = input("input keyword: ")
+# keyword=''
 
 
 def make_url_list(keyword):
@@ -26,102 +28,43 @@ def make_url_list(keyword):
     
     return url_list
 
-# 2  담당업무,자격요건,우대사항 나누기 
+def make_df():
+    df = pd.DataFrame(columns=['url','html'])
+    return df
 
-def crawler():
-    """
-모집부분 및 상세내용
-    공통자격요건
-    담당업무 
-    지원자격
-    우대사항
-각 요건들을 나누기 위해서 html 태그를 기준으로 
-text 앞의 < 뒤의 > 검색해서 태그 검색 
-포함되어있는 태그별로 분류해주기 
-ex <tr>담당업무</tr>일때 담당업무 앞의< ~ 뒤의 > 까지 불러오고
-첫 <에서 가까운 > 를 찾아서 태그 추출 
-해당 tr ~ 다음 tr 나올떄까지 데이터로 분류
+def scraper():
+    base_url = "https://www.saramin.co.kr"
+    url_list = make_url_list(keyword=keyword)
+    total_url = [base_url+add_url for add_url in url_list]
+    detail_page =[]
 
-    """
-    return 
+    for lnk in total_url:   
+        detail_content_num = re.search('rec_idx=(.+?)$',lnk).group(1)
+        detail_page.append(f'https://www.saramin.co.kr/zf_user/jobs/relay/view-detail?rec_idx={detail_content_num}&rec_seq=0&t_category=relay_view&t_content=view_detail&t_ref=&t_ref_content=')
 
+    access_page = []
+    access_link = []
+    for lnk in detail_page:
+        # check=0
+        resp = req.get(lnk,headers=headers)
+        if(resp.status_code==200):
+            page_source = soup(resp.text,"html.parser")
+            contents = page_source.find('div',class_='wrap_tbl_template')
+            if(contents!=None):
+                access_page.append(contents)
+                access_link.append(lnk)
+        else:
+            print(resp.status_code)
 
-
-base_url = "https://www.saramin.co.kr"
-url_list = make_url_list(keyword=keyword)
-total_url = [base_url+add_url for add_url in url_list]
-detail_page =[]
-
-test_url = total_url[:4]
-# print("-000000000000000000000",len(test_url))
-
-for lnk in test_url:   
-    detail_content_num = re.search('rec_idx=(.+?)$',lnk).group(1)
-    detail_page.append(f'https://www.saramin.co.kr/zf_user/jobs/relay/view-detail?rec_idx={detail_content_num}&rec_seq=0&t_category=relay_view&t_content=view_detail&t_ref=&t_ref_content=')
-
-
-# print(detail_page)
-
-for lnk in detail_page:
-    # check=0
-    resp = req.get(lnk,headers=headers)
-    if(resp.status_code==200):
-        page_source = soup(resp.text,"html.parser")
-        contents = page_source.find('div',class_='wrap_tbl_template')
-        # if(contents!=None):
-        #     contents= contents.find('table')
-        # else:
-        #     contents=page_source.find('table',class_="tbl_template")
-        if(contents!=None):
-            conf = contents.text
-            conf_list = conf.split('\n')
-            for c in conf_list:
-                print(c)
-                print('\n'*2)
-        # if(contents!=None):
-        #     # print(contents.text)
-        #     # print('-'*50)
-        #     job_part = contents.find('td')
-        #     if(job_part!=None):
-        #         while(job_part.findNext('tr') != None):  
-        #             job_part = job_part.findNext('tr')
-        #             # print("*"*100)
-        #             job_part_text = job_part.text
-        #             if(check==0):
-        #                 print('지원자격')
-        #                 print(job_part_text)
-        #                 print(lnk)
-        #                 print('-'*50)
-        #             if(check==1):
-        #                 print('우대사항')
-        #                 print(job_part_text)
-        #                 print(lnk)
-        #                 print('-'*50)
-        #             if(job_part_text=='공통 자격요건'):
-        #                 check=1
-        #                 continue
-        #             if(len(job_part_text)==4):
-        #                 if(job_part_text=='지원자격'):
-        #                     check=1
-        #                     continue
-        #                 if(job_part_text=='우대사항'):
-        #                     check=2
-        #                     continue
-        #                 else:
-        #                     check=0
-        #                     continue
-                    
-                        
-
-                            
-            
-
-# 여러개의 포지션을 구인하는 경우 처리를 해주어야 한다.
+    return access_link,access_page
 
 
 
-    else:
-        print(resp.status_code)
 
+df = make_df()
+lnk,page = scraper()
 
-
+df.html=page
+df.url=lnk
+file_name=str(time.time()).replace('.','_')
+df.to_csv(f'.{file_name}.csv',index=True)
--- test.py
+++ test.py
@@ -1,31 +1,19 @@
-import requests
-from bs4 import BeautifulSoup as bs
-import time
-from requests.auth import HTTPBasicAuth
+import re
 
-request_headers = { 'User-Agent' : ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/102.0.1264.37'), } 
-page_count=0
-job_hrefs=['/wd/35398', '/wd/69475', '/wd/105834', '/wd/56969', '/wd/111904', '/wd/111902', '/wd/71968', '/wd/57696', '/wd/63936', '/wd/82150', '/wd/69472', '/wd/83631', '/wd/90288', '/wd/19529', '/wd/114542', '/wd/103663', '/wd/104840', '/wd/118307', '/wd/105502', '/wd/29701', '/wd/98950', '/wd/114923', '/wd/114950', '/wd/96823', '/wd/39278', '/wd/45157', '/wd/56817', '/wd/101290', '/wd/84963', '/wd/111172', '/wd/104027', '/wd/118396', '/wd/108103', '/wd/98019', '/wd/96845', '/wd/106850', '/wd/94077', '/wd/114823', '/wd/76066', '/wd/110569', '/wd/21479', '/wd/113049', '/wd/95932', '/wd/107416']
-base_url2 ="https://www.wanted.co.kr"
-for job_href in job_hrefs:
-    page_count+=1
-    print("-"*50)
-    print("Count:",page_count)
-    print("-"*50)
-    print(base_url2 + job_href)
-    print("-"*50)
-    job_url =base_url2 + job_href
-    response = requests.get( job_url, headers = request_headers,allow_redirects=True)
-    if(response.status_code==200):
-        time.sleep(1)
-        soup_page = bs(response.content, 'html.parser')
-        
-        test = soup_page.select_one('#__next > div.JobDetail_cn__WezJh > div.JobDetail_contentWrapper__DQDB6 > div.JobDetail_relativeWrapper__F9DT5 > div > div.JobContent_descriptionWrapper__SM4UD > section.JobDescription_JobDescription__VWfcb')
-        print(test)
-        # job_title = soup_page.find('section',{"class":"JobHeader_className__HttDA"}).find('h2').text
-        # section = soup_page.find('section',{"class":"JobDescription_JobDescription__VWfcb"}).text
-        # dic_job_scarp[job_title]=section
-    else:
-        print("status_code:",response.status_code)
-    if(page_count>1):
-        break
+text = """        우대사항데이터사이언티스트 0명담당업무ㆍ[담당업무]ㆍ쇼핑플랫폼의 Front-end AI 시스템 개발 (Optimization,CF,FM)ㆍ가격
+비교플랫폼의 Back-end AI 시스템 개발 (NLP,CNN,RNN)[자격요건]ㆍ경력 : 신입 ~ 3년이하ㆍ원활한 커뮤니케이션 능력으로 차분히 논 
+리를 전개하실 수 있는 분ㆍMachine Learning관련 학위 혹은 관련된 프로젝트 경험이 있으신 분ㆍ수작업을 자동화하고 업무를 효율화
+ 하면서 끊임 없는 성능개선에 즐거움을 느끼시는 분ㆍ데이터를 가공/시각화하고 이를 분석하여 인사이트를 얻고 설득하는 것에 즐거
+움을 느끼시는 분[우대사항]ㆍ R,Python,C++ 중 한가지 언어를 능숙하게 다루시는 분ㆍ Keras(Tensorflow) 기반의 NLP분야의 Deep Learning 경험이 있으신 분ㆍ Kaggle 등 머신러닝 대회 참가 경험이 있으신 분
+"""
+
+p=re.compile("(?<=\])(.*?)(?=<\[)")
+# p= re.compile('[\r\n\v]')
+result = p.findall(text)
+print(result)
+# for _ in p.finditer(text):
+#     print()
+#     print(text[_.start():])
+
+
+
