3686cd255dfcb72f105fbd9e6717cb31bb8a2445
yb-nt <notacose@naver.com>
2022-10-25 02:24:35 +0900

    add job_info update

--- saramin.py
+++ saramin.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.122 Whale/3.16.138.27 Safari/537.36'}
 
-keyword = input("input keyword: ")
-# keyword=''
+# keyword = input("input keyword: ")
+keyword='데이터 엔지니어'
 
 
 def make_url_list(keyword):
@@ -63,23 +63,53 @@ def scraper():
                             common = page_text[page_text.find("공통 자격요건")+7:page_text.find("년)")+2]
                         else:
                             common = page_text[page_text.find("공통 자격요건")+7:page_text.find("관")+1]
-                            if("대졸 이상 (4년)S/W F/W 엔지니어S/W팀 0명담당업무" in page_text): # 특정공고 1개에서 에러 발생...
+                            if("대졸 이상 (4년)S/W F/W 엔지니어S/W팀 0명담당업무" in page_text): 
+                                # 특정공고 1개에서 에러 발생...
+                                # 이유를 찾지못해서 string 으로 예외처리
                                 common = page_text[page_text.find("공통 자격요건")+7:page_text.find("년)")+2]
                             else:
                                 pass
-                        
-                    # job_main = page_text[page_text.find('[담당업무]')+6:page_text.find("[자격요건")]
-                    # require = page_text[page_text.find('[자격요건]')+6:page_text.find("[우대사항")]
-                    print("-"*100)
-                    print(lnk)
-                    print("-"*100)
-                    print("common:",common)
-                    print("-"*100)
-                    # print("job_main",job_main)
-                    # print("-"*100)
-                    # print("require:",require)
-                    # print("-"*100)
+                page_text = page_text[len("공통 자격요건")+len(common):]
+                if("자격요건" in page_text):
+                    require = page_text[page_text.find('자격요건')+5:page_text.find("우대")]
+                elif("지원자격" in page_text):
+                    require = page_text[page_text.find('지원자격')+5:page_text.find("우대")]
 
+                require = require.replace("\n","")
+                require = require.replace("            ㆍ기타 필수 사항","")
+                require = require.replace("및","없음")
+
+                if(page_text.find("데이터 엔지니어") !=-1):
+                    lenght = len("데이터 엔지니어")
+                    page_text = page_text[page_text.find("데이터 엔지니어")+lenght:]
+                elif(page_text.find("data engineer") != -1):
+                    lenght = len("data engineer")
+                    page_text = page_text[page_text.find("data engineer")+lenght:]
+                else:
+                    pass
+                
+                if("담당업무" not in page_text):
+                    job_main ="정보 없음"
+                else:
+                    job_main = page_text[page_text.find('담당업무')+5:page_text.find("자격")]
+                    if("우대" in job_main):
+                        job_main = page_text[page_text.find("담당업무")+5:page_text.find("우대")]
+
+                if(len(job_main)<4):
+                    job_main = "정보없음"
+
+
+                print("-"*100)
+                print(lnk)
+                print("-"*100)
+                # # print("common:",common)
+                # print("-"*100)
+               
+                # print("require:",require)
+                # print("-"*100)
+
+                print("job_main",job_main)
+                print("-"*100)
 
 
 
